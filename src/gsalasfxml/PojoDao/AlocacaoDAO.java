3b77f64c88424b67cdae50b42f1a3d277f940217
Yan Rocha <Yan Rocha@DESKTOP-FHMVAOD>
2019-12-07 17:04:57 -0300

    Commit de Criação do Banco

--- src/gsalasfxml/PojoDao/AlocacaoDAO.java
+++ src/gsalasfxml/PojoDao/AlocacaoDAO.java
@@ -1,5 +1,8 @@
 package gsalasfxml.PojoDao;
 
+import java.sql.Connection;
+
 public class AlocacaoDAO {
+    private Connection conexao;
     
 }
---
+++ src/gsalasfxml/conexao_banco/ConexaoSQLite.java
@@ -0,0 +1,53 @@
+package gsalasfxml.conexao_banco;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Danniel Cristie ..
+ */
+public class ConexaoSQLite {
+
+    private Connection conexao;
+
+    /**
+     * Conecta a um banco de dados (cria o banco se ele não existir)
+     *
+     * @return
+     */
+    public boolean conectar() {
+
+        try {
+
+            String url = "jdbc:sqlite:banco_de_dados/banco_sqlite2.db";
+
+            this.conexao = DriverManager.getConnection(url);
+
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+            return false;
+        }
+
+        System.out.println("conectou!!!");
+        
+        return true;
+    }
+
+    public boolean desconectar() {
+
+        try {
+            if (this.conexao.isClosed() == false) {
+                this.conexao.close();
+            }
+
+        } catch (SQLException e) {
+
+            System.err.println(e.getMessage());
+            return false;
+        }
+        System.out.println("desconectou!!!");
+        return true;
+
+    }
+}
