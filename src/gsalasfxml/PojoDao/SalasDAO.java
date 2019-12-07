561aed4f979a39c69be9d740bb0adcbc1bd770da
Yan Rocha <Yan Rocha@DESKTOP-FHMVAOD>
2019-12-07 18:53:59 -0300

    Commit de Criação do Banco

--- src/gsalasfxml/PojoDao/SalasDAO.java
+++ src/gsalasfxml/PojoDao/SalasDAO.java
@@ -1,5 +1,29 @@
 package gsalasfxml.PojoDao;
 
+import gsalasfxml.conexao_banco.ConexaoSQLite;
+import java.sql.Statement;
+
 public class SalasDAO {
+    private final ConexaoSQLite conexaoSQLite;
+    
+    public SalasDAO(ConexaoSQLite aConexaoSQLite){
+        this.conexaoSQLite = aConexaoSQLite;
+    }
+    
+    public void criarTabelaSala(){
+        String sql = "CREATE TABLE IF NOT EXISTS SALA"
+                + "("
+                + "idSALA int primary key,"
+                + "ANDAR int not null,"
+                + "ASA varchar(5),"
+                + "TIPOsala varchar(15),"
+                +")";
+    
+        boolean conectou = this.conexaoSQLite.conectar(); // mensagem de erro na criacao da tabela
+        Statement stmt = this.conexaoSQLite.criarStatement();
+        if(conectou){
+            this.conexaoSQLite.desconectar();
+        }
+    }
     
 }
