d8f1df9ed25efbbe5825a29e9888bcadbc887230
Yan Rocha <Yan Rocha@DESKTOP-FHMVAOD>
2019-12-08 11:04:37 -0300

    Commit da ação Salvar no botão do cadastro da sala

--- src/gsalasfxml/TelasCadastros/TelaCadastroSalaController.java
+++ src/gsalasfxml/TelasCadastros/TelaCadastroSalaController.java
@@ -1,11 +1,14 @@
 package gsalasfxml.TelasCadastros;
 
+import gsalasfxml.PojoDao.Sala;
+import gsalasfxml.PojoDao.SalasDAO;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.input.MouseEvent;
@@ -34,10 +37,29 @@ public class TelaCadastroSalaController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
        btnLimpar.setOnMouseClicked((MouseEvent e)->{
-       acaoLimpar(); 
+       acaoLimpar();
+    });
+       btnSalvar.setOnMouseClicked((MouseEvent s)->{
+       acaoSalvar();
     });
     }    
     @FXML public void acaoLimpar(){
         txtNumS.setText("");
+    }
+    @FXML public void acaoSalvar(){
+        int idSala = Integer.parseInt(txtNumS.getText());
+        
+        RadioButton rAndar = (RadioButton) grupoAndar.getSelectedToggle();
+        String andar = rAndar.getText();
+        
+        RadioButton rAsa = (RadioButton) grupoAsa.getSelectedToggle();
+        String asa = rAsa.getText();
+        
+        RadioButton rTipo = (RadioButton) grupoTipoS.getSelectedToggle();
+        String tipo = rTipo.getText();
+        
+        Sala sala = new Sala(idSala, andar, asa, tipo);
+        SalasDAO dao = new SalasDAO();
+        dao.adicionar(sala);
     }
 }
